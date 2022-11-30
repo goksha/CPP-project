aa299227c854e4e28582bf1af85e2666374063c1
Shashank Gokhale <shashigokhale@gmail.com>
2022-11-30 08:56:38 +0000

    alpha500

--- ecom/settings.py
+++ ecom/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'main.apps.MainConfig',
     'products.apps.ProductsConfig',
+    'classifypro.app.ClassifyproConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
